debeb15903dbaad8169bd6d289da799c9fd6e738
Daniel Côté <dccote@cervo.ulaval.ca>
2026-08-14 00:02:30 -0400

    Update collect_data.py
    Added a cache, and error checking in get_all_files
    Added progress every 2s when its too long
    Added header_properties
    Saving final dataframe as Excel and pickle file.
    Cleaned up the code

--- collect_data.py
+++ collect_data.py
@@ -6,32 +6,51 @@ import subprocess
 import pandas as pd
 import time
 
-def get_all_files(root):
+def get_all_files(root, invisible_files = False, progress = True, use_cache = True):
+    """
+    Get the list of files at a given root directory
+
+    """
+    if not Path(root).exists():
+        raise ValueError(f"The path {root} does not exist")
+
     cache = Path("all_files-cache.txt")
     all_files = []
     
-    if cache.exists():
+    if use_cache and cache.exists():
         with open(cache, "r", encoding="utf-8") as file:
             all_files = file.read().splitlines()
-    else:
+            if all_files[0].startswith(root):
+                print(f"Cache read from {cache}")
+            else:
+                all_files = []
+
+    if not all_files:
+        print("No cache, reading from disk")
+        next_progress_print = time.time() + 2
         for root, dirs, files in os.walk(root):
             for name in files:
-                path = os.path.join(root, name)
-                all_files.append(path)
+                if "/." in name and not invisible_files:
+                    continue
 
-        with open(cache, "w", encoding="utf-8") as file:
-            for line in all_files:
-                file.write(line + "\n")
+                path = os.path.join(root, name)
+                if progress and time.time() > next_progress_print:
+                    print(".", end = "")
+                    next_progress_print = time.time() + 2
+
+                all_files.append(path)
+        print(".")
+
+        if use_cache:
+            with open(cache, "w", encoding="utf-8") as file:
+                for line in all_files:
+                    file.write(line + "\n")
+                print(f"Cache saved to {cache}. Delete the file if you organize the data differently")
 
     return all_files
 
 def extract_properties_from_path(file):
     properties = {}
-
-    # On elimine ce qui n'est pas un fichier de donnees
-    if not file.endswith('.txt'):
-        return {}
-
 
     # Extraction de exp
     pattern = r"exp_?(?P<exp>\d)"
@@ -110,34 +129,51 @@ def extract_properties_from_path(file):
         properties['test'] = True
 
     # Extraction du target/type, si present
-    
     pattern = r"(?P<target>white|blanche|dark|black|verre|gel+ose|anneau|adn)"
     match = re.search(pattern, file,  re.IGNORECASE)
     if match is not None:
         properties.update(match.groupdict())
 
-    # # Fetch file stats
-    # file_info = Path(file).stat()
-    
-    # properties['size_in_bytes'] = file_info.st_size
-    # properties['modification_time'] = datetime.datetime.fromtimestamp(file_info.st_mtime)
     properties['file'] = file
 
     return properties
 
 def extract_extended_properties_from_path(file):
+    """
+    Get more information about the file, but slow
+    """
     extended_properties = {}
 
     # Fetch file stats (slow)
     file_info = Path(file).stat()
     
     extended_properties['size_in_bytes'] = file_info.st_size
-    extended_properties['modification_time'] = datetime.datetime.fromtimestamp(file_info.st_mtime)
+    # Others possible
 
     return extended_properties
 
+def extract_header_from_spectral_file(file):
+    properties = {}
+    with open(file,"r", encoding="utf-8", errors="ignore") as file:
+        first_line = file.readline()
+        if first_line.startswith("Data from"):
+            # It is a Raman spectral file
+            for line in file:
+                line = line.strip()
+                if len(line) > 0:
+                    entry = line.split(":", 1)
+                    if len(entry) == 2 :
+                        properties[f"Spectrum:{entry[0]}"] = entry[1]
+                if ">>>>>Begin Spectral Data<<<<<" in line:
+                    break
+
+    return properties
+
+
 if __name__ == "__main__":
-    root = "/Volumes/Labdata/dcclab/surya"
+    root = "/Volumes/labdata/dcclab/surya" # Pour DCC
+    #root = "." # Directement sur le serveur
+
     all_files = get_all_files(root)
     all_files = [ file for file in all_files if file.endswith('txt') ]
     all_files = [ file for file in all_files if "/." not in file]
@@ -146,21 +182,28 @@ if __name__ == "__main__":
 
     all_properties = []
     count = len(all_files)
-    next_time = time.time() + 2
+    start_time = time.time()
+    next_progress_print = start_time + 2
     for i, file in enumerate(all_files):
-        if time.time() > next_time:
-            print(f"{i} of {count}")
-            next_time += 1
+        if time.time() > next_progress_print:
+            elapsed_time = time.time() - start_time
+            print(f"{i} of {count} [{100*elapsed_time/float(i):.3f}s/ 100 items, {elapsed_time/float(i)*(count-i):.1f}s left]")
+            next_progress_print += 2
 
         properties = extract_properties_from_path(file)
 
-        # This is slow: do only if needed (file size and modification times)
-        #
+        # This is slow: they must open and read the file
+        # header_properties = extract_header_from_spectral_file(file)
+        # properties.update(header_properties)
+
         # extended_properties = extract_extended_properties_from_path(file)
         # properties.update(extended_properties)
 
         all_properties.append(properties)
 
     df = pd.DataFrame(all_properties)
-    df.to_excel("surya-dataset.xlsx", index=False)
+    summary = "surya-dataset-description"
+    df.to_excel(summary+".xlsx", index=False)
+    df.to_pickle(summary+".pkl")
 
+    print(f"Summary written to {summary}")
